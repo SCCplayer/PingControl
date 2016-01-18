0776ec5f7b3c442c8fb5e3011d56a081fd21e009
Kai Kleefsich SWT <sccplayer@googlemail.com>
2016-01-18 14:26:05 +0100

    TestCommit

--- src/de/swt/gui/ViewMain.java
+++ src/de/swt/gui/ViewMain.java
@@ -1,27 +1,7 @@
 package de.swt.gui;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 public class ViewMain extends JFrame {
 
-	JPanel pnlIPv4Eingabe = new JPanel(new FlowLayout());
-	JLabel lblIPv4 = new JLabel("IP-Adresse");
-	JTextField tfIPv4 = new JTextField();
-
-	JButton btnStart = new JButton("Start Ping");
-
-	public ViewMain() {
-		setLayout(new BorderLayout());
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		pnlIPv4Eingabe.add(lblIPv4);
-		pnlIPv4Eingabe.add(tfIPv4);
-
-	}
 }
